ef39d51d301cba7282fa19625e51d407b261b845
rkterungwa16 <terungwakombol@gmail.com>
2019-05-03 19:12:04 +0100

    set up library structure

---
+++ src/Authentication/authenticationTypes.ts
@@ -0,0 +1,15 @@
+interface AuthenticationRequestBody {
+  grant_type: string;
+  username: string;
+  password: string;
+}
+
+interface AuthenticationSuccessResponseBody {
+  access_token: string;
+  token_type: string;
+  username: string;
+  FirstName: string;
+  Id: string;
+  ".issued": Date;
+  ".expires": Date;
+}
